3e23b3b551c2af462a77b63366a57bdc1fd04c85
sp1rale <dimayaremchyk24@gmail.com>
2023-10-12 14:09:48 +0300

    Ex-2 task was done

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print('Nothing')
-print('will work')
-print('unless you do')
+print('"Anyone who')
+print('stops')
+print('learning is old,')
+print('whether at twenty or eighty"')
+print('Henry Ford')
